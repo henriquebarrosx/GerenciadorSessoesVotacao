dcb7389e5d4010f4e6395c0f39a2b891690dee88
Henrique Barros <henriquebarrosx@gmail.com>
2022-05-25 12:34:01 -0300

    Corrige mensagem de validação para voto com valor incorreto

--- src/main/java/com/example/gerenciador_sessoes_votacao/v1/exceptions/InvalidVoteException.java
+++ src/main/java/com/example/gerenciador_sessoes_votacao/v1/exceptions/InvalidVoteException.java
@@ -3,7 +3,7 @@ package com.example.gerenciador_sessoes_votacao.v1.exceptions;
 public class InvalidVoteException extends RuntimeException {
 
     public InvalidVoteException() {
-        super("Vote should only be voted as 'Sim' or 'Não'");
+        super("Vote should only be voted as 'Sim' or 'Nao'");
     }
 
 }
